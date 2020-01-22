67f278a650917fe2bf5eef8574d60ee4fc70b128
Yamatan <oleg.yavorovich@gmail.com>
2020-01-22 23:34:18 +0200

    Replaced DFS with BFS to create new nodes in mini-max algo

--- code/Mimax/Minimax/MinimaxAlgorithm.h
+++ code/Mimax/Minimax/MinimaxAlgorithm.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <queue>
+
 #include "Common/ObjectPool.h"
 #include "Minimax/IMinimaxAlgorithm.h"
 #include "Minimax/IMinimaxResolver.h"
@@ -20,6 +22,13 @@ public:
 
     virtual TTransition Solve(TState const& state, SMinimaxConfig const& config) override;
 private:
+    enum class EOperation
+    {
+        Max = 0,
+        Min,
+        Count
+    };
+    
     struct SNodeEntry
     {
         TTransition m_transition;
@@ -42,9 +51,10 @@ private:
         TTransition m_transition; 
     };
 private:
-    STraversalResult VisitNode(SNode& node, size_t const depth);
+    void BuildTree(SNode& node);
+    STraversalResult VisitNode(SNode& node, EOperation const operation);
     SNode* CreateNode(TState const& parent, TTransition const transition);
-    bool IsBetter(float const newScore, float const currentScore, size_t const depth) const;
+    bool IsBetter(float const newScore, float const currentScore, EOperation const operation) const;
 private:
     CObjectPool<SNode> m_nodesPool;
     IResolver* const m_resolver;
@@ -60,49 +70,72 @@ TTransition CMinimaxAlgorithm<TState, TTransition>::Solve(TState const& state, S
     m_nodesPool.ResetPool(config.m_maxNodesCount);
     auto root = m_nodesPool.AllocateObject(state);
 
-    size_t const depth = 0;
-    auto const result = VisitNode(*root, depth);
+    BuildTree(*root);
+    auto const result = VisitNode(*root, EOperation::Max);
 
     return result.m_transition;
 }
 
 template<typename TState, typename TTransition>
-typename CMinimaxAlgorithm<TState, TTransition>::STraversalResult CMinimaxAlgorithm<TState, TTransition>::VisitNode(SNode& node, size_t const depth)
+void CMinimaxAlgorithm<TState, TTransition>::BuildTree(SNode& node)
+{
+    std::queue<SNode*> currentQueue;
+    std::queue<SNode*> nextQueue;
+    currentQueue.push(&node);
+    
+    for (unsigned int depth = 0; !m_nodesPool.IsEmpty() && depth != m_config.m_maxDepth; ++depth, std::swap(currentQueue, nextQueue))
+    {
+        while (!currentQueue.empty())
+        {
+            SNode& currentNode = *currentQueue.front();
+            currentQueue.pop();
+            auto const transitions = m_resolver->CollectTransitions(currentNode.m_state);
+            if (transitions.empty())
+            {
+                continue;
+            }
+
+            currentNode.m_children.reserve(transitions.size());
+            for (auto const t : transitions)
+            {
+                if (m_nodesPool.IsEmpty())
+                {
+                    break;
+                }
+
+                SNodeEntry entry;
+                entry.m_transition = t;
+                entry.m_node = CreateNode(currentNode.m_state, t);
+                currentNode.m_children.push_back(entry);
+                nextQueue.push(entry.m_node);
+            }
+
+            if (m_nodesPool.IsEmpty())
+            {
+                break;
+            }
+        }
+    }
+}
+
+template<typename TState, typename TTransition>
+typename CMinimaxAlgorithm<TState, TTransition>::STraversalResult CMinimaxAlgorithm<TState, TTransition>::VisitNode(SNode& node, EOperation const operation)
 {
     STraversalResult result;
-    if(depth == m_config.m_maxDepth || m_nodesPool.IsEmpty())
+    if (node.m_children.empty())
     {
-        result.m_score = m_resolver->EvaluateState(node.m_state); 
+        result.m_score = m_resolver->EvaluateState(node.m_state);
         return result;
-    }
-
-    auto const transitions = m_resolver->CollectTransitions(node.m_state);
-    if(transitions.empty())
-    {
-        result.m_score = m_resolver->EvaluateState(node.m_state); 
-        return result;
-    }
-
-    node.m_children.reserve(transitions.size());
-    for(auto const t: transitions)
-    {
-        SNodeEntry entry;
-        entry.m_transition = t;
-        node.m_children.push_back(entry);
     }
 
     bool firstChild = true;
+    EOperation const nextOperation = static_cast<EOperation>((static_cast<int>(operation) + 1) % static_cast<int>(EOperation::Count));
     for(auto& child: node.m_children)
     {
-        child.m_node = CreateNode(node.m_state, child.m_transition);
-        auto const childResult = VisitNode(*child.m_node, depth + 1);
-        if(firstChild)
+        auto const childResult = VisitNode(*child.m_node, nextOperation);
+        if(firstChild && IsBetter(childResult.m_score, result.m_score, operation))
         {
             firstChild = false;
-            result.m_score = childResult.m_score;
-        }
-        else if(IsBetter(childResult.m_score, result.m_score, depth))
-        {
             result.m_transition = child.m_transition;
             result.m_score = childResult.m_score;
         }
@@ -120,9 +153,9 @@ typename CMinimaxAlgorithm<TState, TTransition>::SNode* CMinimaxAlgorithm<TState
 }
 
 template<typename TState, typename TTransition>
-bool CMinimaxAlgorithm<TState, TTransition>::IsBetter(float const newScore, float const currentScore, size_t const depth) const
+bool CMinimaxAlgorithm<TState, TTransition>::IsBetter(float const newScore, float const currentScore, EOperation const operation) const
 {
-    return ((depth & 1) ? newScore < currentScore : newScore > currentScore);
+    return (operation == EOperation::Min ? newScore < currentScore : newScore > currentScore);
 }
 
 #pragma endregion Definition
