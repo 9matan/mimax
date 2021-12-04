d8d3b749ff7b0c53d2d6d90620b58c474815f511
9matan <oleg.yavorovich@gmail.com>
2021-12-04 21:46:40 +0200

    Removed MINIMAX_ENABLE_ALPHA_BETA_PRUNING define

--- code/Mimax/mimax/dma/MinimaxBase.h
+++ code/Mimax/mimax/dma/MinimaxBase.h
@@ -2,50 +2,41 @@
 
 #include <limits>
 
-// Define MINIMAX_ENABLE_ALPHA_BETA_PRUNING (1) to enabled pruning
-#define MINIMAX_ENABLE_ALPHA_BETA_PRUNING (1)
-
 namespace mimax {
 namespace dma {
 
-// TResolver
-// float EvaluateState(TState const&);
-// void GetPossibleMoves(TMovesContainer&, TState const&);
-// void MakeMove(TState&, TMove);
+/*
+TMovesContainer
+    bool empty()
+    iter begin()
+    iter end()
+*/
 
-// TMovesContainer
-// bool empty();
-// begin(), end()
+/*
+TResolver
+    float EvaluateState(TState const&)
+    void GetPossibleMoves(TMovesContainer&, TState const&)
+    void MakeMove(TState&, TMove)
+*/
 
 template<typename TState, typename TMove, typename TMovesContainer, typename TResolver>
 class CMinimaxBase
 {
 public:
     CMinimaxBase(size_t const maxDepth, TResolver const& resolver)
-        : m_maxDepth(maxDepth)
-        , m_resolver(resolver)
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
-        , m_minValue(-std::numeric_limits<float>::max())
-        , m_maxValue(std::numeric_limits<float>::max())
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
+        : CMinimaxBase(maxDepth, resolver, -1.0f, 1.0f)
     {}
 
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
     CMinimaxBase(size_t const maxDepth, TResolver const& resolver, float const minValue, float const maxValue)
         : m_maxDepth(maxDepth)
         , m_resolver(resolver)
         , m_minValue(minValue)
         , m_maxValue(maxValue)
     {}
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
 
     inline TMove Solve(TState const& state)
     {
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
         return VisitState(state, 0, m_minValue, m_maxValue).m_move;
-#else
-        return VisitState(state, 0).m_move;
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
     }
 
 private:
@@ -56,11 +47,7 @@ private:
     };
 
 private:
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
     STraversalResult VisitState(TState const& state, size_t const depth, float alpha, float beta)
-#else
-    STraversalResult VisitState(TState const& state, size_t const depth)
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
     {
         TMovesContainer moves;
         if(depth != m_maxDepth)
@@ -81,19 +68,13 @@ private:
         {
             TState childState = state;
             m_resolver.MakeMove(childState, move);
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
             auto const childResult = VisitState(childState, depth + 1, -beta, -alpha);
-#else
-            auto const childResult = VisitState(childState, depth + 1);
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
             if(-childResult.m_score > result.m_score)
             {
                 result.m_move = move;
                 result.m_score = -childResult.m_score;
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
                 alpha = (result.m_score > alpha) ? result.m_score : alpha;
                 if (alpha >= beta) break;
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
             }
         }
 
@@ -103,10 +84,8 @@ private:
 private:
     TResolver m_resolver;
     size_t m_maxDepth;
-#if MINIMAX_ENABLE_ALPHA_BETA_PRUNING
     float m_minValue;
     float m_maxValue;
-#endif // MINIMAX_ENABLE_ALPHA_BETA_PRUNING
 };
 
 } // dma
